291428400bad5b3fe92f7d509edc45ce30f1225b
Madhu2244 <madhuxsharma@gmail.com>
2021-04-24 15:32:11 -0500

    Location Status

--- Android app/FloodDetection - GFFS/app/src/main/java/com/example/flooddetection/MainActivity.java
+++ Android app/FloodDetection - GFFS/app/src/main/java/com/example/flooddetection/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.flooddetection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Tests");
+    }
+
+    private double[] getLocation() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            return null;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            System.out.println(location.toString());
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            return new double[]{latitude, longitude};
+        }
+        else {
+            return new double[] {33.0198, 96.6989};
+        }
     }
 }
