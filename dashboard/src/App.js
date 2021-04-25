c4f2f2e3400891da376338234211a8f14fdc37ef
Shrey Joshi <shreyjoshi2004@gmail.com>
2021-04-24 23:54:31 -0500

    added lithology to dataset and started work on new dashboard

---
+++ dashboard/src/App.js
@@ -0,0 +1,11 @@
+import DraggableMap from "./DraggableMap"
+
+function App() {
+  return (
+    <div className="App">
+      <DraggableMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard/src/DraggableMap.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+
+const MapComponent = withScriptjs(
+  withGoogleMap((props) => (
+    <GoogleMap defaultZoom={6} center={props.center}>
+          <Marker
+            position={{ lat: 43.397, lng: -80.644 }}
+            icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
+          />
+          <Marker
+            position={{ lat: 43.1385, lng: -79.4845 }}
+            icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
+          />
+          <Marker
+            position={{ lat: 33.0334, lng: -96.6752 }}
+            icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
+          />
+          <Marker
+            position={{ lat: 33.006740, lng: -96.650440 }}
+            icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
+          />
+    </GoogleMap>
+  ))
+);
+
+
+function DraggableMap(props) {
+  return <>
+    <div className="flex-1 h-full overflow-y-hidden">
+      <MapComponent
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAbU6KKAPkJoZiyX1nGH5uC6-TsYBp71AA"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        center={props.currentCenter}
+        markers = {props.locations}
+        />
+    </div>
+  </>
+}
+
+export default DraggableMap;
